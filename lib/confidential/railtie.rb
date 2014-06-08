f69f3a277724193728567d3efb8207f850108183
Mathías Montossi <mmontossi@museways.com>
2014-06-08 18:05:47 -0300

    Improve semantic dependencies

--- lib/confidential/railtie.rb
+++ lib/confidential/railtie.rb
@@ -1,6 +1,6 @@
 module Confidential
   class Railtie < Rails::Railtie
-    
+ 
     config.before_configuration do
       confidential_file = Rails.root.join('config', 'confidential.yml')  
       if File.exist? confidential_file and envs = YAML.load(File.read(confidential_file)) 
