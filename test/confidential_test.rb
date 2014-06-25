89652ebf73d0e80aab9304002349d8658e1b613b
Mathías Montossi <mmontossi@museways.com>
2014-06-24 22:44:06 -0300

    Fix test name

--- test/confidential_test.rb
+++ test/confidential_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ConfidentialTest < ActiveSupport::TestCase
 
-  test "should load confidential values" do
+  test "load confidential values" do
     assert_equal 'user', ENV['DB_USER']
     assert_equal 'pass', ENV['DB_PASS']
   end
